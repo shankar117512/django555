d836e334f9275556e3be426d00fca0c39c87e001
shankar117512 <shankar117512@gmail.com>
2026-05-21 13:46:24 +0530

    Apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,9 +29,9 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-# ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 # base.py — add anywhere in the file
 APPEND_SLASH = True  # Django default, but confirm it's not overridden
